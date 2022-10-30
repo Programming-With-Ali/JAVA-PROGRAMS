1e95723505feac6ad005e6290fc9e2f34df40aa7
Programming-With-Ali <115638875+Programming-With-Ali@users.noreply.github.com>
2022-10-30 07:49:55 -0700

    Create escape_sequence.java

---
+++ escape_sequence.java
@@ -0,0 +1,21 @@
+class escape_sequence {
+    public static void main(String[] args) {
+        System.out.println(" newline escape character");
+        System.out.println("Hey there this Ali\n" + "Ali");
+        System.out.println(" backspace escape character");
+        System.out.println("Hey there this Ali\b");
+        System.out.println(" tab escape character");
+        System.out.println("Hey there this Ali \t");
+        System.out.println(" single qoute escape character");
+        System.out.println("Hey there this Ali \'");
+        System.out.println(" double qoute escape character");
+        System.out.println("Hey there this Ali \"");
+        System.out.println(" This is backslash n used to change line");
+        System.out.println("Hey there this Ali \r");
+        System.out.println("Double qoute \"");
+        System.out.println(" carriage return qoute escape character");
+        System.out.println("Hey there this Ali \\\\");
+        System.out.println(" back slash escape character");
+        System.out.println("Hey there this Ali \\");
+    }
+}
