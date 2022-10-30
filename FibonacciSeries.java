790e39cc12c0eb5240f12b2b40644730bf1471d9
Programming-With-Ali <115638875+Programming-With-Ali@users.noreply.github.com>
2022-10-30 07:36:30 -0700

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,13 @@
+class FibonacciSeries {
+    public static void main(String[] args) {
+        while (true) {
+            int a = 0, b = 1, c;
+            for (int i = 1; i < 13; i++) {
+                System.out.println(" " + a);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
